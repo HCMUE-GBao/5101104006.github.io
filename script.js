fa7f79da61b708169aa23ad47972cb45ab563402
HCMUE-GBao <baory1210@gmail.com>
2025-12-07 23:58:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyA_M3X9VxAOH0jKy799avu09BPA480WHpA",
   authDomain: "hcmue-a95cd.firebaseapp.com",
   projectId: "hcmue-a95cd",
-  storageBucket: "hcmue-a95cd.appspot.com", // recommended format
+  storageBucket: "hcmue-a95cd.firebasestorage.app",
   messagingSenderId: "847360348342",
   appId: "1:847360348342:web:d16d48c63511cd613c1617",
+  measurementId: "G-BRN1DZ4WWX"
 };
 // script.js — Refactor nhẹ bởi Poro (giữ nguyên logic gốc)
 
